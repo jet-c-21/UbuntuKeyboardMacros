b4c7ef475a111b444660817da5f80a032cdfaf9f
Jet (Ta-Wei Chien) <67193495+jet-c-21@users.noreply.github.com>
2024-12-16 16:22:39 +0800

    Add version number to __init__.py

--- ubuntu_keyboard_macros/__init__.py
+++ ubuntu_keyboard_macros/__init__.py
@@ -3,5 +3,7 @@ Author: Jet C.
 GitHub: https://github.com/jet-c-21
 Create Date: 2024-12-15
 """
+__version__ = "0.0.1"
+
 from .event_device_listener import EventDeviceListener
 from .pynput_event_listener import PYNPUTEventListener
